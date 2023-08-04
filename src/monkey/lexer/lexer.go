5fc07a63fb67ea7b3907d06b2e8c3c2c46683daa
GStechschulte <stechschulteg@gmail.com>
2023-08-04 08:18:14 +0200

    readChar() increment readPosition += 1

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -17,6 +17,18 @@ func New(input string) *Lexer {
 	return l
 }
 
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
@@ -40,6 +52,13 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
@@ -51,6 +70,8 @@ func (l *Lexer) readChar() {
 	} else {
 		l.ch = l.input[l.readPosition]
 	}
+	l.position = l.readPosition
+	l.readPosition += 1
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
